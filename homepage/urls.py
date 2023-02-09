0b6d5686a929fbb58c467a03ddb52a2b13cb867b
laravel <laravel@localhost.localdomain>
2023-02-09 05:14:52 -0500

    I have started Implimenting the login logic

--- homepage/urls.py
+++ homepage/urls.py
@@ -5,5 +5,6 @@ urlpatterns  = [
     path('', views.home, name='home-page'),
     path('login', views.login, name='login-page'),
     path('signup', views.signup, name='signup-page'),
+    path('signout', views.logout, name='signout-page'),
     
         ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect 
 from django.urls import reverse
-import requests, json
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -12,6 +13,23 @@ def login(request):
     return render(request, 'registration/login.html')
 
 def signup(request):
+    if request.method == "POST":
+        fname,lname, email,password,confirm_password
+        fname = request.POST["fname"]
+        lanme = request.POST["lname"]
+        email = request.POST["email"]
+        password = request.POST["passwd"]
+        check_box = request.POST["check"]
+
+        if User.objects.filter(email=email).exists():
+            messages.info(request,"Email Address Already Exists")
+            return redirect('signup-page')
+        elif check_box not in request.POST:
+            messages.info(request, "You need to acceptes the terms and conditions")
+        else:
+            user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+            user.save()
+            return redirect('login-page')
     return render(request, 'registration/signup.html')
 
 def logout(request):
